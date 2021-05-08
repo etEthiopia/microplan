5f1e1c3a7f715725c0d23c50c65e26f701dcd322
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2021-05-08 20:08:01 +0300

    redux-store and reducers skelton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Home from './components/dashboard/Home';
 import Landing from './components/landing/Landing';
 import Navbar from './components/layout/Navbar';
+import store from './store/store';
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<div className="App">
-					<Route path="/" exact={true} component={Landing} />
-					<Route path="/login" component={Login} />
-					<Route path="/register" component={Register} />
-					<Route path="/home" component={Home} />
-					
-					{/* <Navbar /> */}
-					{/* <Home /> */}
-				</div>
-			</BrowserRouter>
+			<Provider store={store}>
+				<BrowserRouter>
+					<div className="App">
+						<Route path="/" exact={true} component={Landing} />
+						<Route path="/login" component={Login} />
+						<Route path="/register" component={Register} />
+						<Route path="/home" component={Home} />
+
+						{/* <Navbar /> */}
+						{/* <Home /> */}
+					</div>
+				</BrowserRouter>
+			</Provider>
 		);
 	}
 }
