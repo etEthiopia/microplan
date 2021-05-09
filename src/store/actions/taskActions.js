ec027e2106d47e91d48a885a0121eb20e79bbfef
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2021-05-09 14:25:30 +0300

    + 2 fb helpers, connected them to firebaseConifg

--- src/store/actions/taskActions.js
+++ src/store/actions/taskActions.js
@@ -1,6 +1,7 @@
 export const createTask = (task) => {
-	return (dispatch, getState) => {
+	return (dispatch, getState, {getFirebase, getFirestore}) => {
 		// make async call to the database
+        
 		console.log(task);
 		dispatch({ type: 'CREATE_TASK', payload: task });
 	};
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import { reactReuxFirebase, getFirebase } from 'react-redux-firebase';
+import firebaseConfig from '../config/firebaseConfig';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+	rootReducer,
+	compose(applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))),
+	reduxFirestore(firebaseConfig),
+	reactReuxFirebase(firebaseConfig)
+);
 
 export default store;
